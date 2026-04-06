8379b7b91576e5b822fdbf96224007152ac5c2f5
Altroo <scorpion.yooy@gmail.com>
2026-04-06 10:53:55 +0100

    fix: update BACKEND_SITE_ADMIN URL to point to the correct endpoint

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,6 +1,6 @@
 // Site root
 export const SITE_ROOT = `${process.env.NEXT_PUBLIC_DOMAIN_URL_PREFIX}/`;
-export const BACKEND_SITE_ADMIN = `${process.env.NEXT_PUBLIC_API_URL}/gestion-interne-gf62`;
+export const BACKEND_SITE_ADMIN = `${process.env.NEXT_PUBLIC_API_URL}/gestion-interne-kp56`;
 // Auth
 export const AUTH_LOGIN = `${SITE_ROOT}/login`;
 // Auth forgot password
